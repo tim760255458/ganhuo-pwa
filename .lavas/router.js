0cb02024f8131c14cf2fc6512eedd958f15c7a63
tim760255458 <760255458@qq.com>
2018-12-15 22:21:25 +0800

    update

--- .lavas/router.js
+++ .lavas/router.js
@@ -2,84 +2,84 @@ import Vue from 'vue';
 import Router from 'vue-router';
 
     
-import _15447912092829d49a922ebc8a6d77ca82c0a74289b98 from '@/pages/Appshell.vue';
+import _9d49a922ebc8a6d77ca82c0a74289b98 from '@/pages/Appshell.vue';
     
 
     
-import _1544791209282f7fc7554d202a317883db2ae067316d3 from '@/pages/detail/_id.vue';
+import _f7fc7554d202a317883db2ae067316d3 from '@/pages/detail/_id.vue';
     
 
     
-import _1544791209282d2462dcf0c7beccd286c658e08187914 from '@/pages/Error.vue';
+import _d2462dcf0c7beccd286c658e08187914 from '@/pages/Error.vue';
     
 
     
-import _154479120928267830448037326425509e44bce7632b7 from '@/pages/Index.vue';
+import _67830448037326425509e44bce7632b7 from '@/pages/Index.vue';
     
 
     
-import _15447912092823ab07ba665885c797b127f598464540d from '@/pages/read/_tag.vue';
+import _3ab07ba665885c797b127f598464540d from '@/pages/read/_tag.vue';
     
 
     
-import _1544791209282038de326b0270e97f9eef95b6bc74ed7 from '@/pages/Search.vue';
+import _038de326b0270e97f9eef95b6bc74ed7 from '@/pages/Search.vue';
     
 
     
-import _154479120928293d3a859f5e8f750157ae8381a8c15d8 from '@/pages/type/_type.vue';
+import _93d3a859f5e8f750157ae8381a8c15d8 from '@/pages/type/_type.vue';
     
 
     
-import _154479120928243b75c5eb87bd5901f72e9a192cf7c5b from '@/pages/Xiandu.vue';
+import _43b75c5eb87bd5901f72e9a192cf7c5b from '@/pages/Xiandu.vue';
     
 
 
 let routes = [
     {
         "path": "/appshell",
-        "component": _15447912092829d49a922ebc8a6d77ca82c0a74289b98,
+        "component": _9d49a922ebc8a6d77ca82c0a74289b98,
         "meta": {},
         "name": "appshell"
     },
     {
         "path": "/detail/:id",
-        "component": _1544791209282f7fc7554d202a317883db2ae067316d3,
+        "component": _f7fc7554d202a317883db2ae067316d3,
         "meta": {},
         "name": "detailId"
     },
     {
         "path": "/",
-        "component": _154479120928267830448037326425509e44bce7632b7,
+        "component": _67830448037326425509e44bce7632b7,
         "meta": {},
         "name": "index"
     },
     {
         "path": "/read/:tag",
-        "component": _15447912092823ab07ba665885c797b127f598464540d,
+        "component": _3ab07ba665885c797b127f598464540d,
         "meta": {},
         "name": "readTag"
     },
     {
         "path": "/search",
-        "component": _1544791209282038de326b0270e97f9eef95b6bc74ed7,
+        "component": _038de326b0270e97f9eef95b6bc74ed7,
         "meta": {},
         "name": "search"
     },
     {
         "path": "/type/:type",
-        "component": _154479120928293d3a859f5e8f750157ae8381a8c15d8,
+        "component": _93d3a859f5e8f750157ae8381a8c15d8,
         "meta": {},
         "name": "typeType"
     },
     {
         "path": "/xiandu",
-        "component": _154479120928243b75c5eb87bd5901f72e9a192cf7c5b,
+        "component": _43b75c5eb87bd5901f72e9a192cf7c5b,
         "meta": {},
         "name": "xiandu"
     },
     {
         "path": "/error",
-        "component": _1544791209282d2462dcf0c7beccd286c658e08187914,
+        "component": _d2462dcf0c7beccd286c658e08187914,
         "meta": {},
         "name": "error",
         "alias": "*"
